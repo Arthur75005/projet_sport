99667d0f6fd8bcff7fcd710f0a6208cacc5a40cc
Arthur NICOLAS <a.nicolas28210@gmail.com>
2018-08-09 17:16:11 +0200

    create-page: creation de page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
 {
     public function home()
     {
-        return $this->render("home/index.html.twig");
+        return $this->render("base.html.twig");
     }
     
 }
